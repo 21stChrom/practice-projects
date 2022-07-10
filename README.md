d733a69a7e3cc8bfbd9a24310bf94989c3650484
21stChrom <parkerjmartin2020@gmail.com>
2022-07-09 22:40:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-snrsn2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-snrsn2)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+    import "./style.css";
+    import {FaHome} from '@react-icons/all-files/fa/FaHome';
+    
+    export default function App() {
+      return (
+        <div>
+          <div><FaHome class="home"/></div>
+          <audio controls>
+            
+            </audio>
+        </div>
+      );
+    }
