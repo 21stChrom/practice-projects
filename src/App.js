60707f85701e656e836252b657ce841944fb27b5
21stChrom <parkerjmartin2020@gmail.com>
2022-08-09 19:01:36 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import React from "react";
       return (
         
         <div className="cover">
+          <div className="box">
           <h1 style={{color: "transparent"}}>
           "Don't let anyone, ever make you feel like you don't deserve what you want."</h1>
+          </div>
                <audio controls>     
           <source src="https://cdn.jsdelivr.net/gh/21stChrom/practice-projects@main/src/Juice%20WRLD%20-%20Traumatized%20(%20Lyrics%20)%20[%20Unreleased%20]%20[%20Prod.%20by%20RockyRoadz%20]%20__%20Vibe%20Alone%20[720p].mp3" type="audio/mpeg" />
           </audio>
