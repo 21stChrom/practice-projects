25d1e804f26f0cbb77adc5b3e02f93c087aa59c0
21stChrom <parkerjmartin2020@gmail.com>
2022-08-09 23:28:04 -0400

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 
+import { connect } from 'react-redux';
+import JPlayer from 'react-jplayer';
+import AudioPlayer from "./AudioPlayer/AudioPlayer";
 import React from "react";
     import "./style.css";
     import {FaHome} from '@react-icons/all-files/fa/FaHome';
@@ -9,18 +12,18 @@ import React from "react";
         <div className="cover">
           <div className="box">
           <h1 style={{color: "transparent"}}>
-          "Don't let anyone, ever make you feel like you don't deserve what you want."</h1>
+         </h1>
           </div>
-               <audio controls>     
+               <audio className="audio" controls>     
           <source src="https://cdn.jsdelivr.net/gh/21stChrom/practice-projects@main/src/Juice%20WRLD%20-%20Traumatized%20(%20Lyrics%20)%20[%20Unreleased%20]%20[%20Prod.%20by%20RockyRoadz%20]%20__%20Vibe%20Alone%20[720p].mp3" type="audio/mpeg" />
           </audio>
                       
             <div>
               <div id={"space"} />
-          <div className="abcme"><FaHome className="home"/></div>
-  
+              </div>
+          <div className="abcme">
+            <FaHome className="home"/>
             </div>
-    
         </div>
         
       );
