a44c34f4f1d8e912e5387203534c1f29e73dbdc7
21stChrom <parkerjmartin2020@gmail.com>
2022-08-30 07:04:09 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
+import React from 'react';
+import './style.css';
+import { FaHome } from '@react-icons/all-files/fa/FaHome';
 
-import React from "react";
-    import "./style.css";
-    import {FaHome} from '@react-icons/all-files/fa/FaHome';
-    
-    export default function App() {
-      return (
-        <div class="bb">
-        
-        <div className="cover">
-          <div className="wrapper">
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-</div>
+export default function App() {
+  return (
+    <div className="cover">
+      <div className="wrapper">
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
 
-          <div className="box">
-          
-         
+        <div className="box"></div>
+
+        <div>
+          <div id={'space'}>
+            <input type="" />
           </div>
-              
-            <div>
-              <div id={"space"} >
-                <input type="" />
-              </div>
-              </div>
-          
-        
-              <div className="audio">
-               <audio className="audio" controls> 
-               <source src="https://cdn.jsdelivr.net/gh/21stChrom/practice-projects@main/src/Juice%20WRLD%20-%20I%20Hate%20Wasting%20Time%20%20%20(Unreleased)%20[prod.%20dfk]%20[720p].mp3" type="audio/mpeg" />  
-               </audio>
-               </div>
-               <div>
-               <audio className="audio" controls>  
-               <source src="https://cdn.jsdelivr.net/gh/21stChrom/practice-projects@main/src/Juice%20WRLD%20-%20Traumatized%20(%20Lyrics%20)%20[%20Unreleased%20]%20[%20Prod.%20by%20RockyRoadz%20]%20__%20Vibe%20Alone%20[720p].mp3" type="audio/mpeg" />
+        </div>
+
+        <div className="audio">
+          <audio className="audio" controls>
+            <source
+              src="https://cdn.jsdelivr.net/gh/21stChrom/practice-projects@main/src/Juice%20WRLD%20-%20I%20Hate%20Wasting%20Time%20%20%20(Unreleased)%20[prod.%20dfk]%20[720p].mp3"
+              type="audio/mpeg"
+            />
           </audio>
-             </div>   
-             <button >
-        <span className="home"><FaHome /></span>
-              </button> 
-            
         </div>
+        <div>
+          <audio className="audio" controls>
+            <source
+              src="https://cdn.jsdelivr.net/gh/21stChrom/practice-projects@main/src/Juice%20WRLD%20-%20Traumatized%20(%20Lyrics%20)%20[%20Unreleased%20]%20[%20Prod.%20by%20RockyRoadz%20]%20__%20Vibe%20Alone%20[720p].mp3"
+              type="audio/mpeg"
+            />
+          </audio>
         </div>
-      );
-  }
+        <button>
+          <span className="home">
+            <FaHome />
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+}
